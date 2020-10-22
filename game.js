23af9ba92fba8e54aeab7494d087ddffe260c3c1
WojtasBjj <wojtekgajda94@gmail.com>
2020-10-22 12:08:57 +0200

    poprawa działania efektu losowania ręki komputera

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
+//gdy klikne dwa razy w np papier reka nadal jest wybrana trzeba dac papier.classlist.inculde (.choice)
+
 let playerHand = "";
 let computerHand = 0;
 const papier = document.getElementById('1')
 const kamien = document.getElementById('2')
 const nozyczki = document.getElementById('3')
+
+
+const paperIco = document.createElement("img");
+paperIco.src = "paper.jpg";
+
 const computerHands = [papier, kamien, nozyczki];
 const letsPlay = document.querySelector('button');
 const playerChoice = document.querySelector('.yourChoice');
@@ -12,24 +19,42 @@ let numbers = document.querySelector('.numbersSpan');
 let wins = document.querySelector('.winsSpan');
 let losses = document.querySelector('.lossesSpan');
 let draws = document.querySelector('.drawsSpan');
-const images = ['papier', 'kamień', 'nożyce'];
-numberIndex = 0;
-winsIndex = 0;
-lossesIndex = 0;
-drawsIndex = 0;
-let flaga = true;
+const images = [`papier`, 'kamień', 'nożyce', {
+    img: "images/paper.jpg",
+    text: 'paperICO'}];
 let indexImage = 0;
+
 
     const imageChange = () => {
         // console.log(images[indexImage]);
-        computerChoice.textContent = `Wybór komputera: ${images[indexImage]}`
-        indexImage++;
-        if (indexImage == images.length) {
-            indexImage = 0;
-        }
+        // computerChoice.textContent = `Wybór komputera: ${images[indexImage]}`
+        // indexImage++;
+        // if (indexImage == images.length) {
+        //     indexImage = 0;
+            if (playerHand == papier) {
+                computerChoice.textContent = `Wybór komputera: ${images[indexImage]}`
+                indexImage++;
+                if (indexImage == images.length) {
+                indexImage = 0;
+                }
+            }
+            if (playerHand == kamien) {
+                computerChoice.textContent = `Wybór komputera: ${images[indexImage]}`
+                indexImage++;
+                if (indexImage == images.length) {
+                indexImage = 0;
+                }
+            }
+            if (playerHand == nozyczki) {
+                computerChoice.textContent = `Wybór komputera: ${images[indexImage]}`
+                indexImage++;
+                if (indexImage == images.length) {
+                indexImage = 0;
+                }
+            }
     }
 
-    const setInt = setInterval(imageChange, 200);
+    const setInt = setInterval(imageChange, 300);
 
 const selectImage = (e) => {
     // console.log(e.target);
@@ -59,9 +84,8 @@ const selectImage = (e) => {
 }
 
 const play = (e) => {
-    clearInterval(setInt);
     numbers.textContent++;
-    if (playerHand == "") {
+    if (playerHand.classList == "") {
         alert('wybierz rękę!');
         return;
     }
@@ -133,15 +157,16 @@ const play = (e) => {
 
 
     }
-
-    setTimeout(() => {
-        setInterval(imageChange, 200) 
-    }, 3000);
+    
+    // setTimeout(() => {
+    //     console.log('ruszamy za 3 sec');
+    // }, 3000);
 
     playerHand = "";
     papier.classList.remove('choice');
     kamien.classList.remove('choice');
     nozyczki.classList.remove('choice');
+
 }
 
 letsPlay.addEventListener('click', play)
